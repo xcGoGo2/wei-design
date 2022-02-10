deaff487a15c4a54d3fdf172845e0a491b8ecbdb
chenwei <chenwdzyyx@163.com>
2022-02-10 15:54:38 +0800

    feat：项目添加less、elementUI#1.0.1

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,12 +2,12 @@
  * @Author: chenwei
  * @Date: 2022-02-10 13:30:14
  * @LastEditors: chenwei
- * @LastEditTime: 2022-02-10 13:59:05
+ * @LastEditTime: 2022-02-10 15:04:09
  * @FilePath: \weiManage\src\router\routes.ts
  * @Description: 路由配置
  * 
  */
-import Home from '../components/Home.vue'
+import Home from '../views/Home/index.vue'
 export default [
     { path: '/home', component: Home }
 ]
---
+++ src/views/Home/index.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: chenwei
+ * @Date: 2022-02-10 15:03:44
+ * @LastEditors: chenwei
+ * @LastEditTime: 2022-02-10 15:52:58
+ * @FilePath: \weiManage\src\views\Home\index.vue
+ * @Description: 
+ * 
+-->
+
+<template>
+    <h1>Home</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'Home',
+    components: {},
+    props: {},
+    setup() {
+
+    }
+})
+</script>
+
+
+<style lang="less">
+</style>
