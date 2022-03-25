45be3226ae4c7a93d56a33460b363f003295e536
‘chenwei’ <chenweiwool@163.com>
2022-03-25 12:07:37 +0800

    feat：在style中使用js变量

---
+++ src/components/Layout/Col/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Col',
+    setup() {
+        
+    },
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/Layout/Row/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  name: "Row",
+  props: {
+    // flex-direction
+    direction: {
+      type: String,
+      default: "row",
+    },
+    // align-items
+    align: {
+      type: String,
+      default: "center",
+    },
+    // justify-content
+    justify: {
+      type: String,
+      default: "center",
+    },
+  },
+  setup(props, context) {
+    const { direction, align, justify } = props;
+
+    const rowStyle = reactive({
+        direction,
+      align,
+      justify,
+    })
+    return {
+        rowStyle
+    };
+  },
+});
+</script>
+
+<style lang="less"  scoped>
+.row-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: v-bind("rowStyle.direction");
+  align-items: v-bind("rowStyle.align");
+  justify-content: v-bind("rowStyle.justify");
+}
+</style>
